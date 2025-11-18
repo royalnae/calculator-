1ec29786efb30d661d92c7ca5f7de35c650f90e4
Royal <royalderic9@gmail.com>
2025-11-18 13:38:37 +0530

    program modified

--- calculator.c
+++ calculator.c
@@ -34,4 +34,92 @@ int main() {
 
     printf("Result: %.2lf\n", result);
     return 0;
-}
+#include <stdio.h>
+
+
+
+int main() {
+
+    char operator;
+
+    double num1, num2, result;
+
+
+
+    printf("Enter an operator (+, -, *, /, %%): ");
+
+    scanf(" %c", &operator);
+
+    printf("Enter two numbers: ");
+
+    scanf("%lf %lf", &num1, &num2);
+
+
+
+    switch(operator) {
+
+        case '+':
+
+            result = num1 + num2;
+
+            break;
+
+        case '-':
+
+            result = num1 - num2;
+
+            break;
+
+        case '*':
+
+            result = num1 * num2;
+
+            break;
+
+        case '/':
+
+            if (num2 != 0)
+
+                result = num1 / num2;
+
+            else {
+
+                printf("Error: Division by zero!\n");
+
+                return 1;
+
+            }
+
+            break;
+
+        case '%':
+
+            if ((int)num2 != 0)
+
+                result = (int)num1 % (int)num2;
+
+            else {
+
+                printf("Error: Division by zero!\n");
+
+                return 1;
+
+            }
+
+            break;
+
+        default:
+
+            printf("Error: Invalid operator!\n");
+
+            return 1;
+
+    }
+
+
+
+    printf("Result: %.2lf\n", result);
+
+    return 0;
+
+}}
